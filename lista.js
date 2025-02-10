f4c1367865f11aa8cace0564213f15c39db8c229
EmilyRochaTascaSenai <emily.rocha192@gmail.com>
2025-02-10 09:35:03 -0300

    arrumando erro do js

--- lista.js
+++ lista.js
@@ -1,28 +1,25 @@
-const alunos = ['Emily',10,'10'];
+const alunos = ['Emily', 10, '10'];
 console.log(alunos);
 console.log(alunos[0]);
 
+// Lista vazia (array)
+const notas = [];
 
-//Lista vazia(array)
-const notas=[];
-
-//Aducionar itens na lista
+// Adicionar itens na lista
 notas.push(10);
 notas.push(10);
 notas.push(10);
 notas.push(10);
 notas.push('10');
 
-console.log(notas)
+console.log(notas);
 
-//Somar todas as posições do indice
+// Somar todas as posições do índice
+const soma = notas[0] + notas[1] + notas[3];
 
-const soma=notas[0] + notas[1] + notas[3] 
+console.log(`Soma: ${soma}`);
+console.log(`Qtde. notas cadastradas: ${notas.length}`);
 
-console.log('Soma: ${soma}');
-console.log('Qtde. notas cadastradas:%{notas.length}');
-
-//Media  da soma
-
-const media= soma/notas.length;
-console.log('Média:${media');
+// Média da soma
+const media = soma / notas.length;
+console.log(`Média: ${media}`);
